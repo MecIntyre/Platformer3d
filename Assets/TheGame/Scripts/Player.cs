d567830be9b3ab44ea99704bdc269d76ff194a35
MecIntyre <abuesswalker@gmail.com>
2021-11-30 10:16:26 +0100

    movement code improvement + rotation in view direction

--- Assets/TheGame/Scripts/Player.cs
+++ Assets/TheGame/Scripts/Player.cs
@@ -2,23 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Steuerung der Spielfigur
+// Steuerung der Spielfigur
 public class Player : MonoBehaviour
 {
-    //Laufgeschwindigkeit der Figur.
+    // Laufgeschwindigkeit der Figur.
     public float speed = 0.05f;
 
-    //Grafisches Modell, u.a. für die Drehung in Laufrichtung.
+    // Grafisches Modell, u.a. für die Drehung in Laufrichtung.
     public GameObject model;
+
+    // Der Winkel zu dem sich die Figur um die eigene Achse (=Y) drehen soll
+    private float towardsY = 0f;
 
     // Update is called once per frame
     private void Update()
-    {
-        transform.position += Input.GetAxis("Horizontal") * speed * transform.forward;
+    {   
+        float h = Input.GetAxis("Horizontal");
 
-        if (Input.GetAxis("Horizontal") > 0f)
-            model.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        else if (Input.GetAxis("Horizontal") < 0f)
-            model.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        transform.position += h * speed * transform.forward;
+
+        if (h > 0f) // nach rechts gehen
+            towardsY = 0f;
+        else if (h < 0f) // nach links gehen 
+            towardsY = -180f;
+
+        model.transform.rotation = Quaternion.Lerp(model.transformrotation,Quaternion.Euler(0f, towardsY, 0f), Time.deltaTime * 10f);
     }
 }
