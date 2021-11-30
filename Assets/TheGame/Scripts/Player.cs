15612a3d37e701a3db65c7225ea63bad21f66492
MecIntyre <abuesswalker@gmail.com>
2021-11-30 13:38:50 +0100

    raycast visualization

--- Assets/TheGame/Scripts/Player.cs
+++ Assets/TheGame/Scripts/Player.cs
@@ -59,4 +59,14 @@ public class Player : MonoBehaviour
         }
         rigid.AddForce(new Vector3 (0f, extraGravity, 0f));
     }
+
+    private void OnDrawGizmos() 
+    {
+        if (onGround) Gizmos.color = Color.magenta;
+        else Gizmos.color = Color.green;
+
+        Vector3 rayStartPosition = transform.position + (Vector3.up * 0.1f);
+        Gizmos.DrawLine(rayStartPosition, rayStartPosition + (Vector3.down * 0.12f));
+        
+    }
 }
