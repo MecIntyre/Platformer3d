1f3a9f696c3fa21e61437a7cfd366bb989ffd0d3
MecIntyre <abuesswalker@gmail.com>
2021-12-01 12:17:34 +0100

    add lightbulb changing on activation

--- Assets/TheGame/Scripts/DoorSwitch.cs
+++ Assets/TheGame/Scripts/DoorSwitch.cs
@@ -2,20 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Realisiert, wie die Tür mit einem Schalter, interaktiv geöffnet werden kann.
 public class DoorSwitch : MonoBehaviour
 {
-   
-   public Animator doorAnimator;
-   private void OnTriggerStay(Collider other) 
-   {
-       if (Input.GetAxisRaw ("Fire1") != 0f)    
-       {
-         openTheDoor();     
-       }
-   }
+    // Animator auf dem Tür-mesh, um das öffnen der Tür zu realisieren.
+    public Animator doorAnimator;
 
-   private void openTheDoor()
-   {
-       doorAnimator.SetBool("isOpen", true);
-   }
+    // Zeiger auf das Mesh, das die Lichter an der Schalterkonsole darstellt.
+    public MeshRenderer mesh;
+
+    // Steuert die Tür mittels der Schaltkonsole, wenn die Feuer-Taste gedrückt wird.
+    private void OnTriggerStay(Collider other) 
+    {
+        if (Input.GetAxisRaw ("Fire1") != 0f && !doorAnimator.GetBool("isOpen"))    
+        {
+            openTheDoor();     
+        }
+    }
+
+    // Öffnet die Tür, inklusive Synchronisation von Schalter und Türobjekt.
+    private void openTheDoor()
+    {
+        doorAnimator.SetBool("isOpen", true);
+
+        Material[] mats = mesh.materials;
+        Material m2 = mats[2]; //ausgeschaltetes Material
+        mats[2] = mats[1];
+        mats[1] = m2;
+        mesh.materials = mats;
+    }
 }
