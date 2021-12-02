7fb3f8fdd844c127e9f58404c0529430fb370369
MecIntyre <abuesswalker@gmail.com>
2021-12-02 09:52:54 +0100

    created levelmanager + scene loader and management

--- Assets/TheGame/Scripts/LevelManager.cs
+++ Assets/TheGame/Scripts/LevelManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-   private void Awake() 
-   {
-       SaveGameData.current = SaveGameData.load();
-   }
+    private void Awake() 
+    {
+        SaveGameData.current = SaveGameData.load();
+    }
 
-   private void Update() 
-   {
-       if (Input.GetKeyUp (KeyCode.Alpha1))
-           SaveGameData.current = SaveGameData.load();
-   }
+    public void loadScene(string name)
+    {
+        Debug.Log ("Lade jetzt Szene:" + name);
+        SceneManager.LoadScene(name, LoadSceneMode.Additive);
+    }
+        
+    private void Update() 
+    {
+        if (Input.GetKeyUp (KeyCode.Alpha1))
+            SaveGameData.current = SaveGameData.load();
+    }
 
 }
