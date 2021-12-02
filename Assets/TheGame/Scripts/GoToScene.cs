633744463f4ea7c3f20a21587c222e64d23c88cd
MecIntyre <abuesswalker@gmail.com>
2021-12-02 09:50:54 +0100

    load name script for scene

---
+++ Assets/TheGame/Scripts/GoToScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToScene : MonoBehaviour
+{
+    
+    // Name der Szene, die geladen wird, wenn die Figur den Trigger auslöst.
+    public string scene = "";
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        LevelManager lm = FindObjectOfType<LevelManager> ();
+        lm.loadScene (scene);
+    }
+}
