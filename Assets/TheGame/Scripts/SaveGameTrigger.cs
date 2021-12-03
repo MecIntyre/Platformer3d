5f66b359bb14f2f4a8340bc031d696b00d05fbfb
MecIntyre <abuesswalker@gmail.com>
2021-12-03 12:50:01 +0100

    fixed bug, that barrel or dead player could save the game

--- Assets/TheGame/Scripts/SaveGameTrigger.cs
+++ Assets/TheGame/Scripts/SaveGameTrigger.cs
@@ -14,12 +14,25 @@ public class SaveGameTrigger : MonoBehaviour
         Debug.Log("Jetzt speichern");  
         SaveGameData savegame = SaveGameData.current;
 
-        if (savegame.lastTriggerID != ID)
+        Player p = other.gameObject.GetComponent<Player> ();
+        if (p == null) // Kein Spieler
+        {
+            // Kollision mit anderem Objekt als Spieler -> Ignorieren.
+            return;
+        }
+        else if (p.health <= 0f) // Spieler schon tot
+        {
+            Debug.Log ("Der Spieler hat keine Gesundheitspunkte mehr. Überspringe das Speichern");
+        }
+        else if (savegame.lastTriggerID == ID) //Speicherpunkt schon gespeichert.
+        {
+            Debug.Log ("Dieser Speicherpunkt hat bereits zuletzt gespeichert. Überspringe das Speichern");
+        }
+        else // Speichern möglich
         {
             savegame.lastTriggerID = ID;
             savegame.save ();  
-        } else
-            Debug.Log ("Dieser Speicherpunkt hat bereits zuletzt gespeichert. Überspringe das Speichern");
+        }
     }
 
     private void OnDrawGizmos() 
