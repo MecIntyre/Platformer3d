39f0de87e35824e48699b16c0451287251487940
MecIntyre <abuesswalker@gmail.com>
2021-12-06 08:38:47 +0100

    change commentary

--- Assets/TheGame/Scripts/Utils.cs
+++ Assets/TheGame/Scripts/Utils.cs
@@ -6,7 +6,7 @@ public class Utils
 {
         /// <summary>Zeichnet den Box-Collider.</summary>
         /// <param name="mb">Monobehaviour, das einen Boxcollider als Geschwisterkomponente hat. </param>
-        /// <param name="Color">Farbe des Gizzmos</param>
+        /// <param name="Color">Farbe des Gizmos</param>
    public static void DrawBoxCollider(MonoBehaviour mb, Color color) 
    {
         if (UnityEditor.Selection.activeGameObject != mb.gameObject)
