a2385785881654e04092a1c2b9886dca9ba6e4d9
MecIntyre <abuesswalker@gmail.com>
2021-12-07 19:08:31 +0100

    instantiate explosion for trainer

--- Assets/TheGame/Scripts/Trainer.cs
+++ Assets/TheGame/Scripts/Trainer.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 // Verhalten für die Robo-Kugel
 public class Trainer : BulletCatcher
 {
+    // Prefab, das die Explosion realisiert
+    public GameObject explosionPrototype;
     public override void onHitByBullet()
     {
         base.onHitByBullet();
         Debug.Log ("Trainer zerstört");
-        Destroy (gameObject);
-        
+
+        Instantiate (explosionPrototype, transform.position, transform.rotation);
+
+        gameObject.SetActive (false);
     }
 }
