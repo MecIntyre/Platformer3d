f065234607db845bfb298f45d14d046f4f54a855
MecIntyre <abuesswalker@gmail.com>
2021-12-07 19:06:58 +0100

    outsourced saveme and loadme to superclass

--- Assets/TheGame/Scripts/HealthOrb.cs
+++ Assets/TheGame/Scripts/HealthOrb.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Sammelbare heilungskugel, das Leben erhöht
-public class HealthOrb : Saveable
+public class HealthOrb : SaveableDestructable
 {
     public void OnTriggerEnter(Collider other) 
     {   
@@ -13,31 +13,5 @@ public class HealthOrb : Saveable
             p.health += 0.25f;
             gameObject.SetActive (false);
         }
-    }
-
-    /// Speicher-ID dieses Objekts.
-    public string ID = "";
-
-    protected override void Start()
-    {
-        base.Start();
-        if (ID == "") 
-            Debug.LogError ("Die Healthorb "+gameObject.name+" hat keine ID bekommen!");
-    }
-
-    protected override void saveme(SaveGameData savegame)
-    {
-        base.saveme(savegame);
-
-        if (!gameObject.activeSelf && !savegame.disabledHealthOrbs.Contains(ID)) // wenn deaktiviert und noch nicht gespeichert -> jetzt speichern
-            savegame.disabledHealthOrbs.Add (ID);
-
-    }
-
-    protected override void loadme(SaveGameData savegame)
-    {
-        base.loadme(savegame);
-        if (savegame.disabledHealthOrbs.Contains(ID)) // ich bin in der Liste der deaktivierten -> gameObject jetzt deaktivieren
-        gameObject.SetActive (false);
     }
 }
