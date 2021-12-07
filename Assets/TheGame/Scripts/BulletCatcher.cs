70ee1ae060cae722446ac3145130821e2935c1ae
MecIntyre <abuesswalker@gmail.com>
2021-12-07 10:37:15 +0100

    create bullet catcher logic

---
+++ Assets/TheGame/Scripts/BulletCatcher.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Basis-Script für alle Dinge, die mit der Pistole abgeschossen werden können
+public class BulletCatcher : MonoBehaviour
+{
+   public virtual void onHitByBullet()
+       {
+            Debug.Log(gameObject.name + " wurde von einer Kugel getroffen");
+       }
+}
