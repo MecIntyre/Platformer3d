d4756671c5b385c2a8c290a84c63474524485b2f
MecIntyre <abuesswalker@gmail.com>
2021-12-07 19:04:40 +0100

    outsourced loadme and saveme

--- Assets/TheGame/Scripts/AmmoPack.cs
+++ Assets/TheGame/Scripts/AmmoPack.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Sammelbares Munitionspaket, das Gun.ammo erhöht
-public class AmmoPack : Saveable
+public class AmmoPack : SaveableDestructable
 {
     private void OnTriggerEnter(Collider other) 
     {
@@ -13,30 +13,5 @@ public class AmmoPack : Saveable
             p.GetComponentInChildren<Gun> ().ammo += 5;
             gameObject.SetActive (false);
         }
-    }
-
-    // Speicher-ID dieses Objekts
-    public string ID = "";
-    protected override void Start()
-    {
-        base.Start();
-        if (ID == "")
-            Debug.LogError ("Das AmmoPack "+gameObject.name+" hat keine ID bekommen");
-    }
-
-    protected override void saveme(SaveGameData savegame)
-    {
-        base.saveme(savegame);
-
-        if (!gameObject.activeSelf && !savegame.collectedAmmos.Contains(ID)) // wenn deaktiviert und noch nicht gespeichert -> jetzt speichern
-            savegame.collectedAmmos.Add (ID);
-
-    }
-
-    protected override void loadme(SaveGameData savegame)
-    {
-        base.loadme(savegame);
-        if (savegame.collectedAmmos.Contains(ID)) // ich bin in der Liste der deaktivierten -> gameObject jetzt deaktivieren
-        gameObject.SetActive (false);
     }
 }
