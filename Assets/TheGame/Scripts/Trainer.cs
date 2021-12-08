069e82beae1dcf7c448ceba109a2fed5437b280e
MecIntyre <abuesswalker@gmail.com>
2021-12-08 09:22:57 +0100

    add explosion sound

--- Assets/TheGame/Scripts/Trainer.cs
+++ Assets/TheGame/Scripts/Trainer.cs
@@ -7,12 +7,17 @@ public class Trainer : BulletCatcher
 {
     // Prefab, das die Explosion realisiert
     public GameObject explosionPrototype;
+    
+    // Soundplayer für den Explosionseffekt
+    public GameObject explosionAudioPrototype;
+
     public override void onHitByBullet()
     {
         base.onHitByBullet();
         Debug.Log ("Trainer zerstört");
 
         Instantiate (explosionPrototype, transform.position, transform.rotation);
+        Instantiate (explosionAudioPrototype, transform.position, transform.rotation);
 
         gameObject.SetActive (false);
     }
