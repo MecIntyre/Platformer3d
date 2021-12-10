58a3f00dd3b79fad3d43af5848d7f8212274a2ed
MecIntyre <abuesswalker@gmail.com>
2021-12-10 15:54:54 +0100

    inventory display for UI

---
+++ Assets/TheGame/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Repräsentiert die Daten eines einsammelbaren Objekts
+[CreateAssetMenu]
+public class InventoryItem : ScriptableObject 
+{
+    // Das Bild, das für dieses Objekt angezeigt wird
+    public Sprite uiImage;
+}
+
