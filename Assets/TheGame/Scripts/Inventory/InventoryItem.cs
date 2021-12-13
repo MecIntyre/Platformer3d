796eb2e139b8ee85c0139193a1cae22dfd545487
MecIntyre <abuesswalker@gmail.com>
2021-12-13 14:06:46 +0100

    eddied menu asset name and order

--- Assets/TheGame/Scripts/Inventory/InventoryItem.cs
+++ Assets/TheGame/Scripts/Inventory/InventoryItem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Repräsentiert die Daten eines einsammelbaren Objekts
-[CreateAssetMenu]
+[CreateAssetMenu(menuName="Inventar-Asset", fileName="Neues Inventarobjekt.asset", order=300)]
 public class InventoryItem : ScriptableObject 
 {
     // Das Bild, das für dieses Objekt angezeigt wird
