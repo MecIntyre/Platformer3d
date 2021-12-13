6dd370d852937b816c2b65efec178ef22bc469b9
MecIntyre <abuesswalker@gmail.com>
2021-12-13 14:05:35 +0100

    added keycard aura

--- Assets/TheGame/Scripts/Inventory/KeyCard.cs
+++ Assets/TheGame/Scripts/Inventory/KeyCard.cs
@@ -8,9 +8,25 @@ public class KeyCard : SaveableDestructable
     // Das Inventarobjekt, das den mitgeführten Gegenständen hinzugefügt wird
     public InventoryItem item;
 
+    public GameObject aura;
+
+    public void OnEnable()
+    {
+        aura.SetActive(true);
+    }
+    public void OnDisable()
+    {
+        aura.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other) 
     {
         SaveGameData.current.inventory.add (item);
         gameObject.SetActive (false);
+    }
+
+    public void Update()
+    {
+        transform.Rotate(Vector3.up, 5f, Space.World);
     }
 }
